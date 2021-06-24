92aaca417367819723d88420254952a9f3441d30
duyguulu <d.ulu.duygu@gmail.com>
2021-06-24 21:52:09 +0300

    .

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,5 @@
 
 var apiurl = 'https://localhost:44337/';
-var baseHref = '///C:/Users/MONSTER/Documents/GitHub/Desktop/DashBoard/index.html';
-//file:///C:/Users/MONSTER/Documents/GitHub/CoinDashBoard/src/pages/coin.html
 $(document).ready(function () {
     
     $.getData = function (method, page, _data, callBack) {
@@ -29,23 +27,6 @@ $(document).ready(function () {
             if(err.status===401)
                 window.location.href = "file:///C:/Users/MONSTER/Documents/GitHub/CoinDashBoard/src/pages/login.html";
         });
-    }
-
-    $.getDateFormat= function formatDate(dateString)
-    {
-        // dateString = '2020-04-03 20:30:55';
-        var allDate = dateString.split('T');
-        var thisDate = allDate[0].split('-');
-        var thisTime = allDate[1].split(':');
-        var newDate = [thisDate[2],thisDate[1],thisDate[0] ].join("-");
-
-        var hour = thisTime[0];
-        var hour =hour < 10 ? "" + hour : hour;
-        var min = thisTime[1] ;
-        var sec = thisTime[2] ;
-        var newTime = hour + ':' + min ;
-
-        return newDate + ' ' + newTime;
     }
      
     }
